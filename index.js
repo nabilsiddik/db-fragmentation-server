9e8a1b780622231f0c1bd618c0ac23cd75b60a13
nabilsiddik <nabilsiddik90@gmail.com>
2024-12-11 07:30:49 -0800

    user db setup done

--- index.js
+++ index.js
@@ -6,30 +6,35 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-// Database1 connection
-const database1 = mysql.createConnection({
+// All About Courses_________________________
+
+// course database 1
+const course_db_1 = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'database_1',
+  database: 'course_db_1',
 });
 
-// Database2 connection
-const database2 = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'database_2',
-  });
+// course database 2
+const course_db_2 = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'course_db_2',
+});
 
-database1.connect(err => {
+
+
+// Connect with server
+course_db_1.connect(err => {
   if (err) throw err;
-  console.log('MySQL Connected...');
+  console.log('course database 1 Connected...');
 });
 
-database2.connect(err => {
-    if (err) throw err;
-    console.log('Connected to database_2');
+course_db_2.connect(err => {
+  if (err) throw err;
+  console.log('course database 2 connected');
 });
 
 
@@ -37,38 +42,89 @@ database2.connect(err => {
 // Course Related API
 // Get course
 app.get('/courses', (req, res) => {
-  const query = 'SELECT * FROM courses';
-  database1.query(query, (err, results) => {
+  const query = 'SELECT * FROM course';
+  course_db_1.query(query, (err, results) => {
     if (err) throw err;
     res.json(results);
   });
 });
 
+
+
 // Post course
 app.post('/courses', (req, res) => {
-    const { title, thumbnailUrl, description, price, category } = req.body;
+  const { title, thumbnailUrl, description, price, category } = req.body;
 
-    let targetDatabase;
-    if(price <= 5000){
-        targetDatabase = database1;
-    }else{
-        targetDatabase = database2;
+  let targetDatabase;
+  if (parseInt(price) === 0) {
+    targetDatabase = course_db_1;
+  } else {
+    targetDatabase = course_db_2;
+  }
+
+  const query = 'INSERT INTO course (title, thumbnailUrl, description, price, category) VALUES (?, ?, ?, ?, ?)';
+
+  targetDatabase.query(query, [title, thumbnailUrl, description, price, category], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: 'Failed to add course' });
     }
-  
-    const query = 'INSERT INTO courses (title, thumbnailUrl, description, price, category) VALUES (?, ?, ?, ?, ?)';
-    targetDatabase.query(query, [title, thumbnailUrl, description, price, category], (err, result) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ message: 'Failed to add course' });
-      }
-      res.status(201).json({ message: 'Course added successfully', courseId: result.insertId });
-    });
+    res.status(201).json({ message: 'Course added successfully', courseId: result.insertId });
   });
+});
 
+
+
+
+
+
+
+
+
+// All About Users_________________________
+// user database 1
+const user_db_1 = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'user_db_1',
+});
+
+// user database 2
+const user_db_2 = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'user_db_2',
+});
+
+
+
+// Connect with server
+user_db_1.connect(err => {
+  if (err) throw err;
+  console.log('user database 1 Connected...');
+});
+
+user_db_2.connect(err => {
+  if (err) throw err;
+  console.log('user database 2 connected');
+});
+
+
+
+// Get users
+app.get('/users', (req, res) => {
+  const query = 'SELECT * FROM users';
+  user_db_1.query(query, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
 
 
 app.get('/', (req, res) => {
-    res.send('Server running')
+  res.send('Server running')
 });
 
 
